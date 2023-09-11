a55b87373bdfefaede30822c8090fdf2c6d8e3e0
QuangHuy <huyitcdct.04@gmail.com>
2023-09-11 19:40:10 +0700

    first commit QuangHuy

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-#huy
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+<?php
+echo "My first PHP script! by QHuy";   
+?>
+</body>
+</html>
